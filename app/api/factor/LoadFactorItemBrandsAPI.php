62f82085d7ed6ab421d146deca09d13ad8247e69
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2024-09-15 15:18:29 +0430

    Normalize brand names and add 'OEMAX', 'YULIM', and 'GEO SUNG' brands in LoadFactorItemBrandsAPI.php and LoadFactorItemBrands.php

--- app/api/factor/LoadFactorItemBrandsAPI.php
+++ app/api/factor/LoadFactorItemBrandsAPI.php
@@ -3,7 +3,7 @@
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     header("Location: ../../../views/auth/403.php");
     exit;
-}
+} 
 require_once '../../../config/constants.php';
 require_once '../../../database/db_connect.php';
 require_once '../../../utilities/callcenter/DollarRateHelper.php';
