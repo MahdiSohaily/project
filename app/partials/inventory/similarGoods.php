507eea354d55c48023f3324721369b4b2d6bc2f6
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2024-12-11 09:50:24 +0430

    Add SMS sending functionality

--- app/partials/inventory/similarGoods.php
+++ app/partials/inventory/similarGoods.php
@@ -123,7 +123,10 @@ function getSimilarGoods($factorItems, $billId, $customer, $factorNumber, $facto
 
     if (!empty($selectedGoods) || !empty($lowQuantity)) {
         sendSalesReport($customer, $factorNumber, $factorType, $selectedGoods, $lowQuantity, $billId);
-        (sendPurchaseMessageToCustomer($customer, $factorNumber, $totalPrice, $date));
+    }
+
+    if ($factorType == 0) {
+        sendPurchaseMessageToCustomer($customer, $factorNumber, $totalPrice, $date);
     }
 
     $selectedGoods = [...$selectedGoods, ...$lowQuantity];
