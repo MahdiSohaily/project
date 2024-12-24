ca66e17f191ebb45650d13d8905a1daee4da5ac6
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2024-12-24 11:21:03 +0430

    Add default case to handle additional brands in getSimilarGoods function

--- app/partials/inventory/similarGoods.php
+++ app/partials/inventory/similarGoods.php
@@ -32,6 +32,8 @@ function getSimilarGoods($factorItems, $billId, $customer, $factorNumber, $facto
             case 'کره ای':
                 $ALLOWED_BRANDS[] = 'KOREA';
                 break;
+            default:
+                $ALLOWED_BRANDS[] = $goodNameBrand;
         }
 
         if ($goodNameBrand == 'HIQ' || $goodNameBrand == 'HI') {
